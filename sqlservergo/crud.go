fc1981dab0cf9647d258102da3cb5b970cd2ad56
fretlesskey <jmsnavarro@gmail.com>
2018-01-04 16:37:17 +0800

    Change import values

--- sqlservergo/crud.go
+++ sqlservergo/crud.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-    _ "github.com/denisenkom/go-mssqldb"
-    "database/sql"
-    "context"
-    "log"
+	"context"
+	"database/sql"
 	"fmt"
+	"log"
+
+	_ "github.com/denisenkom/go-mssqldb"
 )
 
 // Replace with your own connection parameters
@@ -19,41 +20,41 @@ var db *sql.DB
 
 func main() {
 
-    // Build connection string
-    connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s;",
-       server, user, password, port, database)
+	// Build connection string
+	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s;",
+		server, user, password, port, database)
 
-    var err error
+	var err error
 
-    // Create connection pool
-    db, err = sql.Open("sqlserver", connString)
-    if err != nil {
-        log.Fatal("Error creating connection pool:", err.Error())
-    }
+	// Create connection pool
+	db, err = sql.Open("sqlserver", connString)
+	if err != nil {
+		log.Fatal("Error creating connection pool:", err.Error())
+	}
 	fmt.Printf("Connected!\n\n")
-	
+
 	// Prepare test table
 	PrepareTestTable()
 
 	// Create employee
 	createID, err := CreateEmployee("Jake", "United States")
-    fmt.Printf("Inserted ID: %d successfully.\n\n", createID)
+	fmt.Printf("Inserted ID: %d successfully.\n\n", createID)
 
-    // Read employees
-    count, err := ReadEmployees()
-    fmt.Printf("Read %d rows successfully.\n\n", count)
+	// Read employees
+	count, err := ReadEmployees()
+	fmt.Printf("Read %d rows successfully.\n\n", count)
 
-    // Update from database
-    updateID, err := UpdateEmployee("Jake", "Poland")
-    fmt.Printf("Updated row with ID: %d successfully.\n\n", updateID)
+	// Update from database
+	updateID, err := UpdateEmployee("Jake", "Poland")
+	fmt.Printf("Updated row with ID: %d successfully.\n\n", updateID)
 
-    // Delete from database
-    rows, err := DeleteEmployee("Jake")
-    fmt.Printf("Deleted %d rows successfully.\n\n", rows)
+	// Delete from database
+	rows, err := DeleteEmployee("Jake")
+	fmt.Printf("Deleted %d rows successfully.\n\n", rows)
 }
 
 // PrepareTestTable function
-func PrepareTestTable()(int64, error) {
+func PrepareTestTable() (int64, error) {
 	ctx := context.Background()
 	var err error
 
@@ -62,146 +63,145 @@ func PrepareTestTable()(int64, error) {
 	}
 
 	// Check if database is alive.
-    err = db.PingContext(ctx)
-    if err != nil {
-        log.Fatal("Error pinging database: " + err.Error())
+	err = db.PingContext(ctx)
+	if err != nil {
+		log.Fatal("Error pinging database: " + err.Error())
 	}
-	
-	tsql := fmt.Sprintf("DROP TABLE IF EXISTS dbo.Employees; " + 
-		"CREATE TABLE dbo.Employees ( " + 
-		"Id INT IDENTITY(1,1) NOT NULL PRIMARY KEY, " + 
-		"Name NVARCHAR(50), " + 
-		"Location NVARCHAR(50)); " + 
-		"INSERT INTO dbo.Employees (Name, Location) VALUES " + 
-		"(N'Jared', N'Australia') ," + 
-		"(N'Nikita', N'India'), " + 
+
+	tsql := fmt.Sprintf("DROP TABLE IF EXISTS dbo.Employees; " +
+		"CREATE TABLE dbo.Employees ( " +
+		"Id INT IDENTITY(1,1) NOT NULL PRIMARY KEY, " +
+		"Name NVARCHAR(50), " +
+		"Location NVARCHAR(50)); " +
+		"INSERT INTO dbo.Employees (Name, Location) VALUES " +
+		"(N'Jared', N'Australia') ," +
+		"(N'Nikita', N'India'), " +
 		"(N'Tom', N'Germany');")
 
-    // Execute non-query
-    result, err := db.ExecContext(ctx, tsql)
-    if err != nil {
-        log.Fatal("Error preparing test table: " + err.Error())
-        return -1, err
-    }
+	// Execute non-query
+	result, err := db.ExecContext(ctx, tsql)
+	if err != nil {
+		log.Fatal("Error preparing test table: " + err.Error())
+		return -1, err
+	}
 
-    return result.LastInsertId()
+	return result.LastInsertId()
 }
 
 // CreateEmployee function
 func CreateEmployee(name string, location string) (int64, error) {
-    ctx := context.Background()
-    var err error
+	ctx := context.Background()
+	var err error
 
-    if db == nil {
-        log.Fatal("What?")
-    }
+	if db == nil {
+		log.Fatal("What?")
+	}
 
-    // Check if database is alive.
-    err = db.PingContext(ctx)
-    if err != nil {
-        log.Fatal("Error pinging database: " + err.Error())
-    }
+	// Check if database is alive.
+	err = db.PingContext(ctx)
+	if err != nil {
+		log.Fatal("Error pinging database: " + err.Error())
+	}
 
-    tsql := fmt.Sprintf("INSERT INTO dbo.Employees (Name, Location) VALUES ('%s','%s');",
-        name, location)
+	tsql := fmt.Sprintf("INSERT INTO dbo.Employees (Name, Location) VALUES ('%s','%s');",
+		name, location)
 
-    // Execute non-query
-    result, err := db.ExecContext(ctx, tsql)
-    if err != nil {
-        log.Fatal("Error inserting new row: " + err.Error())
-        return -1, err
-    }
+	// Execute non-query
+	result, err := db.ExecContext(ctx, tsql)
+	if err != nil {
+		log.Fatal("Error inserting new row: " + err.Error())
+		return -1, err
+	}
 
-    return result.LastInsertId()
+	return result.LastInsertId()
 }
 
 // ReadEmployees function
 func ReadEmployees() (int, error) {
-    ctx := context.Background()
+	ctx := context.Background()
 
-    // Check if database is alive.
-    err := db.PingContext(ctx)
-    if err != nil {
-        log.Fatal("Error pinging database: " + err.Error())
-    }
+	// Check if database is alive.
+	err := db.PingContext(ctx)
+	if err != nil {
+		log.Fatal("Error pinging database: " + err.Error())
+	}
 
-    tsql := fmt.Sprintf("SELECT Id, Name, Location FROM dbo.Employees;")
+	tsql := fmt.Sprintf("SELECT Id, Name, Location FROM dbo.Employees;")
 
-    // Execute query
-    rows, err := db.QueryContext(ctx, tsql)
-    if err != nil {
-        log.Fatal("Error reading rows: " + err.Error())
-        return -1, err
-    }
+	// Execute query
+	rows, err := db.QueryContext(ctx, tsql)
+	if err != nil {
+		log.Fatal("Error reading rows: " + err.Error())
+		return -1, err
+	}
 
-    defer rows.Close()
+	defer rows.Close()
 
-    var count int // = 0
+	var count int // = 0
 
-    // Iterate through the result set.
-    for rows.Next() {
-        var name, location string
-        var id int
+	// Iterate through the result set.
+	for rows.Next() {
+		var name, location string
+		var id int
 
-        // Get values from row.
-        err := rows.Scan(&id, &name, &location)
-        if err != nil {
-            log.Fatal("Error reading rows: " + err.Error())
-            return -1, err
-        }
+		// Get values from row.
+		err := rows.Scan(&id, &name, &location)
+		if err != nil {
+			log.Fatal("Error reading rows: " + err.Error())
+			return -1, err
+		}
 
-        fmt.Printf("\nID: %d, Name: %s, Location: %s", id, name, location)
-        count++
-    }
+		fmt.Printf("\nID: %d, Name: %s, Location: %s", id, name, location)
+		count++
+	}
 
-    return count, nil
+	return count, nil
 }
-
 
 // UpdateEmployee function
 func UpdateEmployee(name string, location string) (int64, error) {
-    ctx := context.Background()
+	ctx := context.Background()
 
-    // Check if database is alive.
-    err := db.PingContext(ctx)
-    if err != nil {
-        log.Fatal("Error pinging database: " + err.Error())
-    }
+	// Check if database is alive.
+	err := db.PingContext(ctx)
+	if err != nil {
+		log.Fatal("Error pinging database: " + err.Error())
+	}
 
-    tsql := fmt.Sprintf("UPDATE dbo.Employees SET Location = @Location WHERE Name= @Name")
+	tsql := fmt.Sprintf("UPDATE dbo.Employees SET Location = @Location WHERE Name= @Name")
 
-    // Execute non-query with named parameters
-    result, err := db.ExecContext(
-        ctx,
-        tsql,
-        sql.Named("Location", location),
-        sql.Named("Name", name))
-    if err != nil {
-        log.Fatal("Error updating row: " + err.Error())
-        return -1, err
-    }
+	// Execute non-query with named parameters
+	result, err := db.ExecContext(
+		ctx,
+		tsql,
+		sql.Named("Location", location),
+		sql.Named("Name", name))
+	if err != nil {
+		log.Fatal("Error updating row: " + err.Error())
+		return -1, err
+	}
 
-    return result.LastInsertId()
+	return result.LastInsertId()
 }
 
 // DeleteEmployee function
 func DeleteEmployee(name string) (int64, error) {
-    ctx := context.Background()
+	ctx := context.Background()
 
-    // Check if database is alive.
-    err := db.PingContext(ctx)
-    if err != nil {
-        log.Fatal("Error pinging database: " + err.Error())
-    }
+	// Check if database is alive.
+	err := db.PingContext(ctx)
+	if err != nil {
+		log.Fatal("Error pinging database: " + err.Error())
+	}
 
-    tsql := fmt.Sprintf("DELETE FROM dbo.Employees WHERE Name=@Name;")
+	tsql := fmt.Sprintf("DELETE FROM dbo.Employees WHERE Name=@Name;")
 
-    // Execute non-query with named parameters
-    result, err := db.ExecContext(ctx, tsql, sql.Named("Name", name))
-    if err != nil {
-        fmt.Println("Error deleting row: " + err.Error())
-        return -1, err
-    }
+	// Execute non-query with named parameters
+	result, err := db.ExecContext(ctx, tsql, sql.Named("Name", name))
+	if err != nil {
+		fmt.Println("Error deleting row: " + err.Error())
+		return -1, err
+	}
 
-    return result.RowsAffected()
+	return result.RowsAffected()
 }
